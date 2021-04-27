364c280b7ceb7213682d4fd5716ce5b83ac70c9f
Niyas Muhammed <niyas.muhammed@qbytez.com>
2021-04-27 11:51:55 +0530

    updates

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Product;
+use App\Favourite;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -19,8 +20,55 @@ class ProductController extends Controller
             $products =  Product::where('category_id', request('category_id'))->get();
             return $products->load('category', 'items', 'images');
         }
+        if(!empty(request('customer_id'))) {
+            $products = Product::all()->load('category', 'items', 'images');
+            $data = [];
+            foreach($products as $key => $product) {
+                $fav_product = Favourite::where('customer_id', request('customer_id'))->where('product_id', $product->id)->first();
+                if(!empty($fav_product)) {
+                    $data[$key]['id'] = $product->id;
+                    $data[$key]['title'] = $product->title;
+                    $data[$key]['description'] = $product->description;
+                    $data[$key]['price_per_unit'] = $product->price_per_unit;
+                    $data[$key]['price_per_palet'] = $product->price_per_palet;
+                    $data[$key]['unit'] = $product->unit;
+                    $data[$key]['qty'] = $product->qty;
+                    $data[$key]['category_id'] = $product->category_id;
+                    $data[$key]['is_favourite'] = true;
+                    $data[$key]['created_at'] = $product->created_at;
+                    $data[$key]['updated_at'] = $product->updated_at;
+                    $data[$key]['category'] = $product->category;
+                    $data[$key]['items'] = $product->items;
+                    $data[$key]['images'] = $product->images;
 
-        return Product::all()->load('category', 'items', 'images', 'favourite');
+                } else {
+                    $data[$key]['id'] = $product->id;
+                    $data[$key]['title'] = $product->title;
+                    $data[$key]['description'] = $product->description;
+                    $data[$key]['price_per_unit'] = $product->price_per_unit;
+                    $data[$key]['price_per_palet'] = $product->price_per_palet;
+                    $data[$key]['unit'] = $product->unit;
+                    $data[$key]['qty'] = $product->qty;
+                    $data[$key]['category_id'] = $product->category_id;
+                    $data[$key]['is_favourite'] = false;
+                    $data[$key]['created_at'] = $product->created_at;
+                    $data[$key]['updated_at'] = $product->updated_at;
+                    $data[$key]['category'] = $product->category;
+                    $data[$key]['items'] = $product->items;
+                    $data[$key]['images'] = $product->images;
+                }
+
+
+            }
+            return $data;
+            if(!empty($fav_products)) {
+                $products =  Product::whereIn('id', $fav_products)->get();
+                return $products->load('category', 'items', 'images', 'favourite');
+
+            }
+        }
+
+        return Product::all()->load('category', 'items', 'images');
     }
 
     /**
