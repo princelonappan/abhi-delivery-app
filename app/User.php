92d5863bf14daafb7a512176eed4d10476a8aa5b
Niyas Muhammed <niyas.muhammed@qbytez.com>
2021-05-01 14:53:43 +0530

    field update

--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'userable_type', 'userable_id'
+        'name', 'email', 'password', 'userable_type', 'userable_id', 'facebook_id', 'google_id'
     ];
 
     /**
