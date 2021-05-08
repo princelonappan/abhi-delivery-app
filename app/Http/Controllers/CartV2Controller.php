6913a0887cf9466ebb6156270116b3b45cab6aeb
Niyas Muhammed <niyas.muhammed@qbytez.com>
2021-05-08 10:04:04 +0530

    update

--- app/Http/Controllers/CartV2Controller.php
+++ app/Http/Controllers/CartV2Controller.php
@@ -32,7 +32,7 @@ class CartV2Controller extends Controller
             $delivery_charge = 0;
 
             // $delivery_charge = !empty($delivery) ? (($delivery->min_amount > $product_total) ? $delivery->min_amount : 0)  : 0;
-            $delivery_charge = !empty($delivery) ? (($delivery->amount > $product_total) ? $delivery->amount : 0)  : 0;
+            $delivery_charge = !empty($delivery) ? (($delivery->min_amount > $product_total) ? $delivery->amount : 0)  : 0;
             $is_payment_mode = Settings::where('slug', 'payment_mode')->first();
 
             $data['product_total'] = $product_total;
@@ -88,7 +88,7 @@ class CartV2Controller extends Controller
             $delivery = $vat = Settings::where('slug', 'delivery_charge')->first();
             $delivery_charge = 0;
 
-            $delivery_charge = !empty($delivery) ? (($delivery->amount > $product_total) ? $delivery->amount : 0)  : 0;
+            $delivery_charge = !empty($delivery) ? (($delivery->min_amount > $product_total) ? $delivery->amount : 0)  : 0;
             $is_payment_mode = Settings::where('slug', 'payment_mode')->first();
 
             $data['product_total'] = $product_total;
